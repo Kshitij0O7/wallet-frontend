0a2b7d3b8a9aa8bf830f8298be324da89e8de1bc
Kshitij <kshitij.mahajan.20042@iitgoa.ac.in>
2023-08-30 00:11:37 +0530

    Stripe Payment Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+import CheckoutForm from './components/checkoutForm';
 
-function App() {  
 
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const stripePromise = loadStripe('pk_test_51Nk7AESAeLqr2N5IuEFzt6a1vgHTj2AtS8KZSvEIkhovUPJevZW3tjDdGRM456DxdrMA1n8tsKXczBk3kWBeQTMq00rApdBlm1');
 
+const options = {
+  mode: 'payment',
+  amount: 1099,
+  currency: 'inr',
+  // Fully customizable with appearance API.
+  appearance: {
+    /*...*/
+  },
+};
+
+const App = () => (
+  <Elements stripe={stripePromise} options={options}>
+    <CheckoutForm />
+  </Elements>
+);
+
+ReactDOM.render(<App />, document.body);
 export default App;
